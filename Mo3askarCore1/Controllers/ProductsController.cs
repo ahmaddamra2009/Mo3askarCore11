1d0dde887d2823d518a27397d4c63f3db70e8a06
Ahmad Damra <ahmeddamra_cs@hotmail.com>
2021-03-03 21:36:43 +0200

    Add project files.

---
+++ Mo3askarCore1/Controllers/ProductsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mo3askarCore1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mo3askarCore1.Controllers
+{
+
+    public class ProductsController : Controller
+    {
+        // Pro = table
+        static List<Product> pro = new List<Product>() {
+        new Product{Id=1,ProductName="IPhonex",Price=1000,Qty=50},
+        new Product{Id=2,ProductName="Nokia",Price=500,Qty=70}
+       };
+
+        public IActionResult AllProducts()
+        {
+            // from iActionResult to view
+            ViewBag.pName = "Note 20";
+            ViewBag.pPrice = "900";
+
+            return View(pro);
+        }
+
+        public IActionResult NewProduct()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            // select *
+            // from product
+            // where Id=id
+            // LINQ
+            var x = (from p in pro
+                     where p.Id == id
+                     select p).SingleOrDefault();
+            return View(x);
+
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                pro.Add(product);
+                return RedirectToAction("AllProducts");
+            }
+            return View(product);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var x = (from y in pro
+                     where y.Id == id
+                     select y).SingleOrDefault();
+            pro.Remove(x);
+            return RedirectToAction("AllProducts");
+        }
+
+
+    }
+}
---
+++ Mo3askarCore1/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mo3askarCore1.Models
+{
+    // Code First ( Entity FWCore)
+    public class Product
+    {
+
+        /// <summary>
+        /// Id >> Primary Key + Identity
+        /// </summary>
+        /// [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Enter Product Name")]
+        [MinLength(4,ErrorMessage ="Product name g.t 4 char")]
+        [Display(Name ="Product Name")]
+        public string ProductName { get; set; }
+        [Range(100,1000,ErrorMessage ="price between 100 - 1000")]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Enter Qty ")]
+        public int Qty { get; set; }
+
+    }
+}
